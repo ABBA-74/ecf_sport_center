dba81b41a48cb96378171d0b152d35558cd7bcd4
Abdel Bailoul <ab.bailoul@gmail.com>
2022-08-26 20:28:55 +0200

    added webpack encore + config sass + bootstrap + stimulus.js

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $test = 'Page of test';
+        return $this->render('pages/home/index.html.twig', [
+            'test' => $test
+        ]);
+    }
+}
